c7aaa965836cce5bfac0faed0b78d5800f5ac432
Joanna Baile <joanna@studentsofferingsupport.org>
2019-03-29 13:01:24 -0400

    style updated

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ const config = {
     user: "root",
     password: "root",
     host: "localhost",
-    port: "8889",
+    //port: "3306", //this is my host
+    port: "8889", //default port for mamp users
     database: "db_favouritethings"
 }
 
